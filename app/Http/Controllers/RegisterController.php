56b41e31799009ba15a58153fe1db148182d6923
Irullatif <irulsoekamti94@gmail.com>
2021-08-19 11:22:02 +0700

    PEsan Registrasi Success

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -29,8 +29,9 @@ class RegisterController extends Controller
         $validatedData['password'] = Hash::make($validatedData['password']);
         
         User::create($validatedData);
-        $request->session()->flash('success', 'Registration successfull! Please login');
 
-        return redirect('/login');
+        // $request->session()->flash('success', 'Registration successfull! Please login');
+
+        return redirect('/login')->with('success', 'Registration successfull! Please login');
     }
 }
--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -5,9 +5,9 @@
 <div class="row justify-content-center">
   <div class="col-md-4">
 
-    @if (session()->has('succes'))
+    @if (session()->has('success'))
       <div class="alert alert-success alert-dismissible fade show" role="alert">
-        <strong>{{ session('success') }}</strong>
+        {{ session('success') }}
         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
       </div>
     @endif
