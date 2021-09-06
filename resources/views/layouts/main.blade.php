f443485302062ed741c239254f352bda2b3a3355
Irullatif <irulsoekamti94@gmail.com>
2021-09-07 00:07:30 +0700

    Update Font

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -10,6 +10,9 @@
     {{-- Bootstrap Icons --}}
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
 
+    {{-- <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,900;1,700&display=swap" rel="stylesheet"> --}}
     {{-- My Css --}}
     <link rel="stylesheet" href={{ asset("/css/style.css") }}>
 
